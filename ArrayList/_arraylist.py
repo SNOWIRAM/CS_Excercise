cc727101ed150785b8bba3ca6bfdbd2875e60b4e
SnowIRAM <ds9927@g.skku.edu>
2020-09-18 17:41:46 +0900

    Array List 1st trial

--- ArrayList/_arraylist.py
+++ ArrayList/_arraylist.py
@@ -1,6 +1,6 @@
 from _array import Array
-?
-?
+
+
 class ArrayList:
     """
     if ... write necessary code.
@@ -10,33 +10,85 @@ class ArrayList:
         append O(1), pop O(N), remove O(N), insert O(N)
     """
     def __init__(self):
-        ...
-?
+        self._size = 0
+        self._array = Array(size=10)
+
     def __getitem__(self, index):
-        # Don't touch
         if not 0 <= index < self._size:
             raise IndexError
         return self._array[index]
-?
+
     def __setitem__(self, index: int, value):
-        # Don't touch
         if not 0 <= index < self._size:
             raise IndexError
         self._array[index] = value
-?
+
     def __repr__(self):
-        # Don't touch
         return f"ArrayList[{', '.join(str(self._array[i]) for i in range(self._size))}]"
-?
+
     def __len__(self):
-        # O(1)
-        return ...
-?
+        return self._size
+
     def __contains__(self, item: int):
-        # al = ArrayList()À ¶§, item in al ±â ±¸Ç
-        # O(N)
-        ...
-        return ...
-?
+        bool_contain = False
+        for i in self._array:
+            if i == item:
+                bool_contain = True
+                break
+        return bool_contain
+
+    def append(self, value):
+        if self._size == self._array._size:
+            temp_array = Array(size=2*self._array._size)
+
+            for i in range(self._array._size):
+                temp_array[i] = self._array[i]
+            self._array = temp_array
+
+        self._array[self._size] = value
+        self._size += 1
+
+    def pop(self, index: int):
+        self._size -= 1
+        value = self._array[index]
+
+        for i in range(index, self._size):
+            self._array[i] = self._array[i+1]
+
+        return value
+
+    def remove(self, index: int):
+        self._size -= 1
+
+        for i in range(index, self._size):
+            self._array[i] = self._array[i+1]
+
+    def insert(self, index: int, value):
+        if self._size == self._array._size:
+            temp_array = Array(size=2*self._array._size)
+            for i in range(self._array._size):
+                temp_array[i] = self._array[i]
+            self._array = temp_array
+
+        self._size += 1
+        for j in range(self._size, index, -1):
+            self._array[j] = self._array[j-1]
+
+        self._array[index] = value
+
+
 # Aside from arraylist
 # 1. Binary Search - social distancing problem
+
+if __name__ == "__main__":
+    al = ArrayList()
+    for i in range(200):
+        al.append(i)
+    print(al)
+    print(len(al))
+    al.pop(1)
+    print(al)
+    print(len(al))
+    al.insert(4, 2)
+    print(al)
+    print(len(al))
